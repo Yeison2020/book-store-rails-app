49976957dc6d4436afc020bef8fcd819bb2e7610
Yeison <enmanuelquiterios@gmail.com>
2021-11-22 10:44:28 -0500

    First commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,18 @@
+class BooksController < ApplicationController
+    def list  
+    end
+    def show 
+
+    end
+    def new
+
+    end
+
+    def create 
+
+    end
+
+    def edit
+
+    end
+end
